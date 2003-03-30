c6b5acc8ec788a57f769c7fbdac7cff48aeddfdc
parasyte <parasyte>
2003-03-30 07:54:27 +0000

    branch 1.0

--- dev/alpha-1/fcms.php
+++ dev/alpha-1/fcms.php
@@ -5,9 +5,16 @@ include("lib/libfcms.inc");
 //initilize: connect to db and set variables 
 fcms_init();
 
+//set host and site headers
+//do_host_header();
+//do_site_header();
+
+
 //hand off the functionality to the library based on $mode
 fcms_run();
 
-//insert a siteguide footer
+//insert footers (page, siteguide, host)
 do_page_footer();
+//do_site_footer();
+//do_host_footer();
 ?>
